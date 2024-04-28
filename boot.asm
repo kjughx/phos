463f313942cc00c7316c10c994d20226ea188f4d
Philip J. <kjughx@gmail.com>
2024-04-28 12:27:39 +0200

    Clean up boot.asm

--- boot.asm
+++ boot.asm
@@ -11,21 +11,6 @@ times 33 db 0
 step1:
     jmp 0x7c0:step2
 
-
-handle_zero:
-    mov ah, 0eh
-    mov al, 'A'
-    mov bx, 0x00
-    int 0x10
-    iret
-
-handle_one:
-    mov ah, 0eh
-    mov al, 'V'
-    mov bx, 0x00
-    int 0x10
-    iret
-
 step2:
     cli ; Disable interrupts
     mov ax, 0x7c0
@@ -36,16 +21,6 @@ step2:
     mov sp, 0x7c00
     sti ; Enable interrupts
 
-    mov word[ss:0x00], handle_zero
-    mov word[ss:0x02], 0x7c0
-
-    mov word[ss:0x04], handle_one
-    mov word[ss:0x06], 0x7c0
-
-    int 1
-
-    mov si, message
-    call print
     jmp $
 
 print:
@@ -65,8 +40,6 @@ print_char:
     int 0x10
 
     ret
-
-message: db 'Hello, World!', 0
 
 times 510-($ - $$) db 0
 dw 0xAA55
